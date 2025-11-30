e3db45fa030d35c915835425960ac11cdfdac33c
sureshmagnolia <118750471+sureshmagnolia@users.noreply.github.com>
2025-11-30 21:17:12 +0530

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -4,5 +4,6 @@ import react from '@vitejs/plugin-react'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
+  // CRITICAL: Matches your repo name 'timetabling'
   base: '/timetabling/', 
 })
