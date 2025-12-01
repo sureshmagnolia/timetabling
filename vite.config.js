89b9541bb5a91977f651e47dec41cbebb7910f21
sureshmagnolia <118750471+sureshmagnolia@users.noreply.github.com>
2025-12-01 11:48:30 +0530

    Delete vite.config.js

--- vite.config.js
+++
@@ -1,9 +0,0 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
-
-// https://vitejs.dev/config/
-export default defineConfig({
-  plugins: [react()],
-  // CRITICAL: Matches your repo name 'timetabling'
-  base: '/timetabling/', 
-})
